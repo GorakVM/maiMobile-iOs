d698cebe58b2a674efec1270f7f6488cf03aa2da
Gil Lopes <gil_lopes@live.com.pt>
2016-04-19 12:06:39 +0100

    Created XMLParser

---
+++ maiMobile-iOS/XMLParser.swift
@@ -0,0 +1,46 @@
+//
+//  XMLParser.swift
+//  maiMobile-iOS
+//
+//  Created by Gil Lopes on 12/04/16.
+//  Copyright © 2016 WATERDOG mobile. All rights reserved.
+//
+
+import Foundation
+
+class XMLParser: NSObject, NSXMLParserDelegate {
+    
+    var dictionary = [String : AnyObject]()
+    var charactersFound = ""
+    var elementFound = false
+    var array = [AnyObject]()
+    
+    init(data: NSData, handler: (AnyObject) -> Void) {
+        super.init()
+        let parser = NSXMLParser(data: data)
+        parser.delegate = self
+        parser.parse()
+        
+        handler(array)
+    }
+    
+    func parser(parser: NSXMLParser, didStartElement elementName: String, namespaceURI: String?, qualifiedName qName: String?, attributes attributeDict: [String : String]) {
+        elementFound = true
+        charactersFound.removeAll()
+    }
+    
+    func parser(parser: NSXMLParser, foundCharacters string: String) {
+//        print("foundCharacters: \(string)")
+        if elementFound {
+            charactersFound += string
+        }
+    }
+    
+    func parser(parser: NSXMLParser, didEndElement elementName: String, namespaceURI: String?, qualifiedName qName: String?) {
+        dictionary[elementName] = charactersFound
+        array.append(dictionary)
+        dictionary.removeAll()
+        elementFound = false
+    }
+    
+}
