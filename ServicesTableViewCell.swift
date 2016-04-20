182d074b0248a74c1cedd461c7a73fe10fef7539
Gil Lopes <gil_lopes@live.com.pt>
2016-04-20 16:14:37 +0100

    created nib for Services

---
+++ ServicesTableViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  ServicesTableViewCell.swift
+//  maiMobile-iOS
+//
+//  Created by Gil Lopes on 20/04/16.
+//  Copyright © 2016 WATERDOG mobile. All rights reserved.
+//
+
+import UIKit
+
+class ServicesTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var imageview: UIImageView!
+    @IBOutlet weak var noteLabel: UILabel!
+    
+}
