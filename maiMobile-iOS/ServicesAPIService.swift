74bf637c65ebd0e952ea145076a32f34e4e9bf9c
Gil Lopes <gil_lopes@live.com.pt>
2016-04-20 12:35:05 +0100

    updated services endPoint

--- maiMobile-iOS/ServicesAPIService.swift
+++ maiMobile-iOS/ServicesAPIService.swift
@@ -10,7 +10,7 @@ import Foundation
 
 class ServicesAPIService {
     
-    private let baseURLString = "https://freedatagenerator.s3.amazonaws.com/pk0gSSJ5kUkM.xml"
+    private let baseURLString = "https://freedatagenerator.s3.amazonaws.com/bPYySe1W2rbA.xml"
     
     // for protection in case that the parser gets called multiple times
     private var parsers = [XMLParser]()
