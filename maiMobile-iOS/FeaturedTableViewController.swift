52f535b5295c381a47df6844a67f9023c72faee1
Gil Lopes <gil_lopes@live.com.pt>
2016-04-20 12:38:35 +0100

    created featuredTableViewController

---
+++ maiMobile-iOS/FeaturedTableViewController.swift
@@ -0,0 +1,34 @@
+//
+//  FeaturedTableViewController.swift
+//  maiMobile-iOS
+//
+//  Created by Gil Lopes on 19/04/16.
+//  Copyright © 2016 WATERDOG mobile. All rights reserved.
+//
+
+import UIKit
+
+class FeaturedTableViewController: ServiceController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    // MARK: - Table view data source
+    
+    override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return services.count
+    }
+    
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("Cell", forIndexPath: indexPath)
+        cell.textLabel?.text = services[indexPath.section].title
+        return cell
+    }
+    
+}
