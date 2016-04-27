7fdde71f960e48647cc52ff9843b038c9ad1878f
Gil Lopes <gil_lopes@live.com.pt>
2016-04-27 14:35:20 +0100

    created Psp NSManagedObject class

---
+++ Psp.swift
@@ -0,0 +1,35 @@
+//
+//  PSP.swift
+//  maiMobile-iOS
+//
+//  Created by Gil Lopes on 22/04/16.
+//  Copyright © 2016 WATERDOG mobile. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+
+extension Psp {
+    
+    @NSManaged var address: String?
+    @NSManaged var desc: String?
+    @NSManaged var district: String
+    @NSManaged var latitude: NSNumber
+    @NSManaged var longitude: NSNumber
+    @NSManaged var name: String?
+    @NSManaged var phone: String?
+    @NSManaged var remoteId: NSNumber?
+    @NSManaged var township: String
+    @NSManaged var comand: String?
+    @NSManaged var postalCode: String?
+    @NSManaged var abbreviation: String?
+    
+}
+
+
+class Psp: NSManagedObject {
+
+// Insert code here to add functionality to your managed object subclass
+
+}
