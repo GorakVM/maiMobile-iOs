63e90e3adb8624c474fb4c59c06de7d8f78830f5
Gil Lopes <gil_lopes@live.com.pt>
2016-04-27 14:25:42 +0100

    Created MapViewController

---
+++ maiMobile-iOS/MapViewController.swift
@@ -0,0 +1,23 @@
+//
+//  MapViewController.swift
+//  maiMobile-iOS
+//
+//  Created by Gil Lopes on 22/04/16.
+//  Copyright © 2016 WATERDOG mobile. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+class MapViewController: UIViewController {
+
+    @IBOutlet weak var mapView: MKMapView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+    
+
+}
