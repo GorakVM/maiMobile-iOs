dec5533ff5ab8119516e3f814d9e56fe9c16e627
Gil Lopes <gil_lopes@live.com.pt>
2016-05-10 15:34:27 +0100

    created Utils file

---
+++ maiMobile-iOS/Utils.swift
@@ -0,0 +1,47 @@
+//
+//  GnrPinImage.swift
+//  maiMobile-iOS
+//
+//  Created by Gil Lopes on 29/04/16.
+//  Copyright © 2016 WATERDOG mobile. All rights reserved.
+//
+
+import UIKit
+
+extension UIImage {
+    
+    enum HierarchyType: String {
+        case CentroDeCooperacaoPolicialAduaneira = "CENTRO DE COOPERA«√O POLICIAL E ADUANEIRA"
+        case ComandoTerritorial = "COMANDO TERRITORIAL"
+        case DestacamentoDeIntervencao = "DESTACAMENTO DE INTERVEN«√O"
+        case DestacamentoDeTransito = "DESTACAMENTO DE TR¬NSITO"
+        case DestacamentoTerritorial = "DESTACAMENTO TERRITORIAL"
+        case PostoDeTransito = "POSTO DE TR¬NSITO"
+        case PostoFiscal = "POSTO FISCAL"
+        case PostoTerritorial = "POSTO TERRITORIAL"
+        case SubdestacamentoTerritorial = "SUBDESTACAMENTO TERRITORIAL"
+        
+    }
+    
+    class func setImageToAnnotation(hierarchyType: HierarchyType.RawValue) -> UIImage {
+        var image = UIImage()
+        
+        switch hierarchyType {
+        case HierarchyType.CentroDeCooperacaoPolicialAduaneira.rawValue: image = UIImage(named: "gnrBlack")!
+        case HierarchyType.ComandoTerritorial.rawValue: image = UIImage(named: "gnrBlue")!
+        case HierarchyType.DestacamentoDeIntervencao.rawValue: image = UIImage(named: "gnrBlueGreen")!
+        case HierarchyType.DestacamentoDeTransito.rawValue: image = UIImage(named: "gnrGreen")!
+        case HierarchyType.DestacamentoTerritorial.rawValue: image = UIImage(named: "gnrGrey")!
+        case HierarchyType.PostoDeTransito.rawValue: image = UIImage(named: "gnrOrange")!
+        case HierarchyType.PostoFiscal.rawValue: image = UIImage(named: "gnrPurple")!
+        case HierarchyType.PostoTerritorial.rawValue: image = UIImage(named: "gnrRed")!
+        case HierarchyType.SubdestacamentoTerritorial.rawValue: image = UIImage(named: "gnrYellow")!
+        default:
+            break
+        }
+        
+        return image
+    }
+    
+  
+}
