c65ec86273fdb0818dcb02e20feec807401fae6f
Gil Lopes <gil_lopes@live.com.pt>
2016-05-10 15:59:50 +0100

    added user location to MapViewController

--- maiMobile-iOS/MapViewController.swift
+++ maiMobile-iOS/MapViewController.swift
@@ -8,16 +8,60 @@
 
 import UIKit
 import MapKit
+import CoreLocation
 
-class MapViewController: UIViewController {
-
+class MapViewController: UIViewController, CLLocationManagerDelegate, MKMapViewDelegate, NSFetchedResultsControllerDelegate {
+    
     @IBOutlet weak var mapView: MKMapView!
+    
+    var locationManager = CLLocationManager()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        
+        locationManager.delegate = self
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.pausesLocationUpdatesAutomatically = true
+        locationManager.distanceFilter = 500
+        
+        let isLocationServicesEnabled = CLLocationManager.locationServicesEnabled()
+        
+        if isLocationServicesEnabled {
+            locationManager.startUpdatingLocation()
+        }
+        
+        
+        mapView.delegate = self
+        mapView.showsUserLocation = true
+        
     }
-
     
-
+    func mapViewDidFinishLoadingMap(mapView: MKMapView) {
+        locationManager.stopUpdatingLocation()
+    }
+    
+    func mapView(mapView: MKMapView, didUpdateUserLocation userLocation: MKUserLocation) {
+        print("userLocation: \(userLocation)")
+        
+    }
+    
+    func mapView(mapView: MKMapView, regionDidChangeAnimated animated: Bool) {
+    }
+    
+    func mapView(mapView: MKMapView, viewForAnnotation annotation: MKAnnotation) -> MKAnnotationView? {
+        
+        return nil
+    }
+    
+    func locationManager(manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        let location = locations.last!
+        let span = MKCoordinateSpanMake(0.25, 0.25)
+        let center = CLLocationCoordinate2D(latitude: location.coordinate.latitude, longitude: location.coordinate.longitude)
+        let region = MKCoordinateRegion(center: center, span: span)
+        mapView.setRegion(region, animated: true)
+        
+    }
+    
 }
