513e590bc04667e8f132cae5e0af476b4dfa8aa5
Gil Lopes <gil_lopes@live.com.pt>
2016-05-10 15:41:05 +0100

    Created DetailTableViewCell

---
+++ maiMobile-iOS/DetailTableViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  DetailTableViewCell.swift
+//  maiMobile-iOS
+//
+//  Created by Gil Lopes on 02/05/16.
+//  Copyright © 2016 WATERDOG mobile. All rights reserved.
+//
+
+import UIKit
+
+class DetailTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var subtitleLabel: UILabel!
+    @IBOutlet weak var detailImageView: UIImageView!
+
+}
