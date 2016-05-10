872a7ee5aa90d64417fff59a8d767aea77a1942b
Gil Lopes <gil_lopes@live.com.pt>
2016-05-10 15:09:44 +0100

    deleted necessary code

--- ServiceController.swift
+++ ServiceController.swift
@@ -9,33 +9,12 @@
 import UIKit
 import CoreData
 
-class ServiceController: UITableViewController, NSFetchedResultsControllerDelegate {
-    
-    var fetchResultController: NSFetchedResultsController!
-    var result = [Service]()
-    var services = [Service]()
-    var featuredServices = [Service]()
+class ServiceController: UITableViewController {
     
     let cellNibIdentifier = "serviceCell"
     
     override func viewDidLoad() {
         tableView.rowHeight = CGFloat(103)
-        tableView.registerNib(UINib(nibName: "ServicesTableViewCell", bundle: nil), forCellReuseIdentifier: "serviceCell")
-        let fetchRequest = NSFetchRequest(entityName: "Service")
-        fetchRequest.sortDescriptors = [NSSortDescriptor(key: "position", ascending: true)]
-        fetchResultController = NSFetchedResultsController(fetchRequest: fetchRequest, managedObjectContext: Fetcher.sharedMainContext(), sectionNameKeyPath: nil, cacheName: nil)
-        try! fetchResultController.performFetch()
-        fetchResultController.delegate = self
-        result = fetchResultController.fetchedObjects as! [Service]
-        
-        for service in result {
-            if service.highlight {
-                featuredServices.append(service)
-            } else {
-                services.append(service)
-            }
-        }
         
     }
-    
 }
