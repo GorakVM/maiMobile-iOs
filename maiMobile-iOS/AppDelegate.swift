7a688a1bb93b5d96ae0cfc9fc76f2d5561ba835a
Gil Lopes <gil_lopes@live.com.pt>
2016-05-11 18:47:00 +0100

    missing code

--- maiMobile-iOS/AppDelegate.swift
+++ maiMobile-iOS/AppDelegate.swift
@@ -17,7 +17,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         // Override point for customization after application launch.
     
-        UINavigationBar.appearance().barTintColor = UIColor.bananaColor
+        UINavigationBar.appearance().barTintColor = UIColor.bananaColor()
         
         Fetcher().loadServicesToCoreData()
         Fetcher().loadGnrToCoreData()
