7e81024b7b02ed0a3caed9ae8d936aa911940eef
Gil Lopes <gil_lopes@live.com.pt>
2016-05-19 18:32:09 +0100

    Created PSpTableViewCell

---
+++ maiMobile-iOS/PspTableViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  PspTableViewCell.swift
+//  maiMobile-iOS
+//
+//  Created by Gil Lopes on 17/05/16.
+//  Copyright © 2016 WATERDOG mobile. All rights reserved.
+//
+
+import UIKit
+
+class PspTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var subtitleLabel: UILabel!
+    @IBOutlet weak var rightDetailLabel: UILabel!
+
+}
