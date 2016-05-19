aeec19b0bc6b0b1f4a8e0f0f2760bee3b1705223
Gil Lopes <gil_lopes@live.com.pt>
2016-05-19 18:22:57 +0100

    created GnrTableViewCell

---
+++ maiMobile-iOS/GnrTableViewCell.swift
@@ -0,0 +1,16 @@
+//
+//  GnrTableViewCell.swift
+//  maiMobile-iOS
+//
+//  Created by Gil Lopes on 17/05/16.
+//  Copyright © 2016 WATERDOG mobile. All rights reserved.
+//
+
+import UIKit
+
+class GnrTableViewCell: UITableViewCell {
+    
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var rightDetailLabel: UILabel!
+    
+}
