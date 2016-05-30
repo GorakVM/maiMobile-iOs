3bc8086c7737ca4175c6361937a253c6633fcce0
Gil Lopes <gil_lopes@live.com.pt>
2016-05-30 19:49:31 +0100

    refactor servicesAPI class for Json parser

--- maiMobile-iOS/ServicesAPIService.swift
+++ maiMobile-iOS/ServicesAPIService.swift
@@ -10,10 +10,7 @@ import Foundation
 
 class ServicesAPIService {
     
-    private let baseURLString = "https://freedatagenerator.s3.amazonaws.com/W553GJLpgH8Y.xml"
-    
-    // for protection in case that the parser gets called multiple times
-    private var parsers = [XMLParser]()
+    private let baseURLString = "https://api.myjson.com/bins/ygq0"
     
     private func request() -> NSMutableURLRequest {
         let url = NSURL(string: baseURLString)!
@@ -23,22 +20,21 @@ class ServicesAPIService {
         return request
     }
     
-    func getDictionaryFromData(completion: (AnyObject) -> Void)  {
+    func getDictionaryFromData(completion: ([AnyObject]) -> Void)  {
         let task = NSURLSession.sharedSession().dataTaskWithRequest(request()) { (data, response, error) in
-            guard let httpResponse = response as? NSHTTPURLResponse, xmlData = data else {
+            guard let httpResponse = response as? NSHTTPURLResponse, jsonData = data else {
                 print("ServicesAPIService response: \(response) error: \(error)")
                 return
             }
             
             if httpResponse.statusCode == 200 {
-                let parser = XMLParser(data: xmlData, handler: { (objectArray) in
+                let parser = JSONParser(data: jsonData, handler: { (objectArray) in
                     completion(objectArray)
                 })
-                self.parsers.append(parser)
                 
             } else {
                 print("servicesAPIService statusCode: \(httpResponse.statusCode)")
-                print("data: \(xmlData)")
+                print("data: \(jsonData)")
             }
             
         }
