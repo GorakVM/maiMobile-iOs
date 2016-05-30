63395c98ad82b34e23cf3af206b11d55bfd6bef7
Gil Lopes <gil_lopes@live.com.pt>
2016-05-30 19:54:31 +0100

    added barButtonItem images to SecurityBaseController

--- maiMobile-iOS/SecurityBaseController.swift
+++ maiMobile-iOS/SecurityBaseController.swift
@@ -19,10 +19,13 @@ class SecurityBaseController: UIViewController {
     @IBAction func barButtonItem(sender: UIBarButtonItem) {
         var nextViewController = UIViewController()
         barButtonItem.enabled = false
+        barButtonItem.image = nil
         if (currentViewController === childViewControllers.first!) {
             nextViewController = childViewControllers.last!
+            barButtonItem.image = UIImage(named: "mapBarButtonItem")
         } else if (currentViewController === childViewControllers.last!) {
             nextViewController = childViewControllers.first!
+            barButtonItem.image = UIImage(named: "bulletListBarButtonItem")
         }
         
         transitionFromViewController(currentViewController, toViewController: nextViewController, duration: NSTimeInterval.abs(1), options: .TransitionFlipFromRight, animations: nil, completion: { (finished) in
@@ -35,9 +38,12 @@ class SecurityBaseController: UIViewController {
     }
     
     override func viewDidLoad() {
+        let titleString = NSAttributedString(string: "Segurança", attributes:
+            [NSFontAttributeName: UIFont(name: "AvenirNext-Regular", size: 15)!])
+        title = titleString.string
         let mapViewController = storyboard!.instantiateViewControllerWithIdentifier("MapViewController") as! MapViewController
         let securityTableViewController = storyboard!.instantiateViewControllerWithIdentifier("SecurityTableViewController") as! SecurityTableViewController
-        
+        barButtonItem.image = UIImage(named: "bulletListBarButtonItem")
         currentViewController = mapViewController
         addChildViewController(mapViewController)
         addChildViewController(securityTableViewController)
