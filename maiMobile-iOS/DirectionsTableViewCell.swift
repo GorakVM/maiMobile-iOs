7f8aa74d2bb863f6ba6350521c2b7231deeefb44
Gil Lopes <gil_lopes@live.com.pt>
2016-06-01 18:11:46 +0100

    Created directionsTableViewCell

---
+++ maiMobile-iOS/DirectionsTableViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  DirectionsTableViewCell.swift
+//  maiMobile-iOS
+//
+//  Created by Gil Lopes on 01/06/16.
+//  Copyright © 2016 WATERDOG mobile. All rights reserved.
+//
+
+import UIKit
+
+class DirectionsTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var subtitleLabel: UILabel!
+    @IBOutlet weak var detailImageView: UIImageView!
+    @IBOutlet weak var imageLabel: UILabel!
+    
+}
