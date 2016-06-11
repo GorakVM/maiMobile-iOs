dddb78331a05842e47dbfbaa8b1f26ffe070b2ff
Gil Lopes <gil_lopes@live.com.pt>
2016-06-11 18:29:02 +0100

    renamed imageView

--- ServicesTableViewCell.swift
+++ ServicesTableViewCell.swift
@@ -11,7 +11,7 @@ import UIKit
 class ServicesTableViewCell: UITableViewCell {
 
     @IBOutlet weak var titleLabel: UILabel!
-    @IBOutlet weak var imageview: UIImageView!
+    @IBOutlet weak var leftImageView: UIImageView!
     @IBOutlet weak var noteLabel: UILabel!
     
 }
