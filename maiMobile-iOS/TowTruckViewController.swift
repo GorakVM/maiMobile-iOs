2d2306a3dd7a743deae821b2a101224750ec7862
Gil Lopes <gil_lopes@live.com.pt>
2016-06-11 18:34:05 +0100

    set textfields logic

--- maiMobile-iOS/TowTruckViewController.swift
+++ maiMobile-iOS/TowTruckViewController.swift
@@ -18,24 +18,46 @@ class TowTruckViewController: UIViewController, UITextFieldDelegate {
     
     @IBOutlet weak var searchButton: UIButton!
     
+    @IBOutlet weak var scrollView: UIScrollView!
+    
+    var plateNumber = ""
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+
+        if let navigationBarHeight = navigationController?.navigationBar.frame.height, tabBarHeight = tabBarController?.tabBar.frame.height {
+            scrollView.contentInset = UIEdgeInsetsMake(navigationBarHeight, 0.0, tabBarHeight, 0.0)
+        }
+        
         searchButton.layer.cornerRadius = CGFloat(8)
+        
+        textFieldFirstTwindDigits.delegate = self
+        textFieldSecondTwinDigits.delegate = self
+        textFieldThirdTwinDigits.delegate = self
         
         textFieldsMaskView.layer.cornerRadius = CGFloat(8)
         textFieldsMaskView.layer.borderColor = UIColor.whiteColor().CGColor
         textFieldsMaskView.layer.borderWidth = CGFloat(1)
-//        textFieldFirstTwindDigits.becomeFirstResponder()
-        
-    }
-    
-    
-    func textFieldDidBeginEditing(textField: UITextField) {
+        textFieldFirstTwindDigits.becomeFirstResponder()
         
     }
     
     func textField(textField: UITextField, shouldChangeCharactersInRange range: NSRange, replacementString string: String) -> Bool {
+        if textField.text?.characters.count > 1 {
+            textField.resignFirstResponder()
+        }
+        
         return true
+    }
+    
+    func textFieldDidEndEditing(textField: UITextField) {
+        switch textField {
+        case textFieldFirstTwindDigits: textFieldSecondTwinDigits.becomeFirstResponder()
+        case textFieldSecondTwinDigits: textFieldThirdTwinDigits.becomeFirstResponder()
+        default:
+            break
+            
+        }
     }
     
     @IBAction func searchButtonAction(sender: UIButton) {
