a2fb125b4f5097dfabcbfe0b70e0677c5640d416
GilLopesDev <gil_lopes@live.com.pt>
2016-06-19 17:59:44 +0100

    Created SecuritySearchResultViewController

---
+++ maiMobile-iOS/SecuritySearchResultViewController.swift
@@ -0,0 +1,56 @@
+//
+//  SecuritySearchResultViewController.swift
+//  maiMobile-iOS
+//
+//  Created by Gil Lopes on 26/05/16.
+//  Copyright © 2016 WATERDOG mobile. All rights reserved.
+//
+
+import UIKit
+import CoreData
+
+class SecuritySearchResultViewController: UITableViewController, UISearchResultsUpdating {
+    
+    var forces = [Force]()
+    var filteredForces = [Force]()
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        
+    }
+    
+    override func viewDidLoad() {
+        
+        print("view.frame.origin: \(view.frame.origin)")
+        tableView.rowHeight = CGFloat(80)
+        tableView.registerNib(UINib(nibName: "GnrTableViewCell", bundle: nil), forCellReuseIdentifier: "gnrCell")
+        tableView.registerNib(UINib(nibName: "PspTableViewCell", bundle: nil), forCellReuseIdentifier: "pspCell")
+    }
+    
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return filteredForces.count
+    }
+    
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("gnrCell") as! GnrTableViewCell
+        let filteredForce = filteredForces[indexPath.row]
+        switch filteredForce.forceType {
+        case "gnr": return SecurityHelper.getCellForGnrTypeAtIndexPathWithForceObject(tableView, indexPath: indexPath, force: filteredForce)
+        case "psp": return SecurityHelper.getCellForPspTypeAtIndexPathWithForceObject(tableView, indexPath: indexPath, force: filteredForce)
+        default: break
+        }
+        
+        return cell
+    }
+    
+    func updateSearchResultsForSearchController(searchController: UISearchController) {
+        let filter = forces.filter { $0.name.localizedCaseInsensitiveContainsString(searchController.searchBar.text!)
+        }
+        filteredForces = filter
+        tableView.reloadData()
+        
+    }
+    
+    
+    
+}
